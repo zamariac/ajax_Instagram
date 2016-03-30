0fc9c110276aaf723b1b4ea01f8d89e5aac40dc2
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-30 18:26:43 -0500

    images posting

--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,88 @@
-// $('.dropdown').hide();
+//this is the dropdown menu
 
-// $('.plus').click(function(e){
-// 	e.preventDefault();
+$('.dropdown').hide();   
 
-// 	if ($('.dropdown').is(':visible')){
-// 		$('dropdown').hide()
-// 	}
-// 	else{
-// 		$('.dropdown').show()
-// 	}
+$('.plus').click(function(e){
+	e.preventDefault();
 
-// });
+	if ($('.dropdown').is(':visible')){
+		$('.dropdown').toggle("hide")
+	}
+	else{
+		$('.dropdown').toggle("show")
+	}
 
-function postImages(){
+});
+
+//this is grabbing the input url and sending it to heroku
+
+$('.add-image').click(function(e){
+				e.preventDefault();
+				
+				var imageUrl = $('.image-url').val();
+				var imageCaption = $('.image-caption').val();
+
+				postImage(imageUrl, imageCaption);
+			
+				
+			});
+
+collectionUrl = 'http://small-tiyfe.herokuapp.com/collections/maria-ig';
+
+//this ajax request  is posting to heroku
+function postImage(image, caption){
 	var addImages = {
-		url: 'http://small-tiyfe.herokuapp.com/collections/maria-ig/',
+		url: collectionUrl,
 		type: 'POST',
 		dataType: 'json',
+		data: {imageUrl:image, imageCaption:caption},
 		success: function(data){
-			
-			
-
-			$('.add-image').click(function(e){
-				e.preventDefault();
-
-				var imageUrl = $('.input-url');
-
-				$('<img src="' + imageUrl + '">').load(function(){
-				$(this).appendTo.$('.image-place');
-				console.log(imageUrl);
-
-
-					})
-			})
-
-			
 		},
 		
 		error: function(error){
 			console.log(error);
-		}
-
+		} 
 
 	};
-
+	console.log(image);
 
 	$.ajax(addImages);
 }
 
-
-function onGetSuccess(data){
-	
-
-
-
-}
-
-function onGetError(data){
-
-}
-
-
+//this  ajax request is getting images from heroku for user to see
 var ajaxSettings = {
-	url: 'http://small-tiyfe.herokuapp.com/collections/maria-ig/',
-	type: 'GET',
+	url: collectionUrl, 
+	type: 'GET',   
 	dataType: 'json',
-	success: onGetSuccess,
-	error: onGetError
+	success: function(data){
+		//var container = $('.image-place');
+		//var textBox = $('.image-caption-place');
+
+
+		console.log(data);
+
+		for (i=0; i < data.length; i++){
+
+		console.log(data[i]);
+
+		var image = data[i].imageUrl;
+		var caption = data[i].imageCaption;
+		console.log(caption);
+
+		 $('<img src="' + image + '">').appendTo('.image-place');
+		
+		 $("<div class='new-div'>" + caption + "</div>").appendTo('.image-place');
+
+			
 };
+
+
+},
+
+
+	error: function(){
+	},
+	
+}
 
 $.ajax(ajaxSettings);
